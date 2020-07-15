680f1b23a0410ceffedbbc5bb281626e67a73911
Vishal Badyal <38772150+Lonewolf92@users.noreply.github.com>
2020-07-15 19:33:55 -0400

    Update Cut generating LP.jl

--- Julia JuMP applications/Cut generating LP.jl
+++ Julia JuMP applications/Cut generating LP.jl
@@ -2,36 +2,33 @@ using JuMP, Gurobi
 
 cglp = Model(Gurobi.Optimizer)
 
-A1 = [-1 0 3]
-B1 = [3 1 2]
-A2 = [-1 0 4]
-B2 = [3 -1 9]
+dim =  2
+A1 = [-1 3; 0 1; 3 2]
+A2 = [-1 3; 0 -1; 4 9]
 b1 = [8 3 30]
 b2 = [8 -4 60]
 xstar = [150/19 60/19]
 
-@variable(cglp, alphaplus >= 0)
-@variable(cglp, betaplus >= 0)
-@variable(cglp, alphaminus >= 0)
-@variable(cglp, betaminus >= 0)
-@variable(cglp, alphazero)
+@variable(cglp, alphaplus[1:dim] >= 0)
+@variable(cglp, alphaminus[1:dim] >= 0)
+@variable(cglp, beta)
 @variable(cglp, u[1:3] >= 0)
 @variable(cglp, v[1:3] >= 0)
 
-@objective(cglp, Max, sum((alphaplus - alphaminus) * xstar[1] + (betaplus-betaminus) * xstar[2]) - alphazero)
+@objective(cglp, Max, sum((alphaplus[j] - alphaminus[j]) * xstar[j] for j = 1:dim) - beta)
 
-
-@constraint(cglp, sum(u[i] * A1[i] for i=1:3) >= (alphaplus - alphaminus))
-@constraint(cglp, sum(v[i] * A2[i] for i=1:3) >= (alphaplus - alphaminus))
-@constraint(cglp, sum(u[i] * B1[i] for i=1:3) >= (betaplus - betaminus))
-@constraint(cglp, sum(v[i] * B2[i] for i=1:3) >= (betaplus - betaminus))
-@constraint(cglp, sum(v[i] * b2[i] for i=1:3) <= alphazero)
-@constraint(cglp, sum(u[i] * b1[i] for i=1:3) <= alphazero)
-@constraint(cglp, (betaplus + betaminus) + (alphaplus + alphaminus) == 1)
+@constraint(cglp, Constr1[j in 1:dim], sum(u[i] * A1[i,j] for i=1:3) >= (alphaplus[j] - alphaminus[j]))
+@constraint(cglp, Constr2[j in 1:dim], sum(v[i] * A2[i,j] for i=1:3) >= (alphaplus[j] - alphaminus[j]))
+@constraint(cglp, sum(u[i] * b1[i] for i=1:3) <= beta)
+@constraint(cglp, sum(v[i] * b2[i] for i=1:3) <= beta)
+@constraint(cglp, sum((alphaplus[j] + alphaminus[j]) for j = 1:dim) == 1)
 
 optimize!(cglp)
 
-println("The objective value is=", objective_value(cglp))
-println("alpha=", value(alphaplus) - value(alphaminus))
-println("beta=", value(betaplus) - value(betaminus))
-println("alphazero=", value(alphazero))
+println("The objective value is = ", objective_value(cglp))
+
+for j in 1:dim
+println("alpha$j = ", value(alphaplus[j] - alphaminus[j]))
+end
+
+println("beta = ", value(beta))
