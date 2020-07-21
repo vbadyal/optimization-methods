bef2b05336e9f4302ef63c5c68d4886bbf928220
Vishal Badyal <38772150+Lonewolf92@users.noreply.github.com>
2020-07-20 22:49:48 -0400

    Split cuts using Cut Generating LP (CGLP)
    Comments added

--- Julia JuMP applications/Cut generating LP.jl
+++ Julia JuMP applications/Cut generating LP.jl
@@ -3,13 +3,13 @@ using JuMP, Gurobi
 cglp = Model(Gurobi.Optimizer)
 
 dim =  2
-A1 = [-1 3; 0 1; 3 2]
+A1 = [-1 3; 0 1; 3 2]    #Use a split disjunction of your choice, using (y<=3 v y>=4) here. 
 A2 = [-1 3; 0 -1; 4 9]
 b1 = [8 3 30]
 b2 = [8 -4 60]
-xstar = [150/19 60/19]
+xstar = [150/19 60/19] # Fractional point to be cut off using split cut (x,y)
 
-@variable(cglp, alphaplus[1:dim] >= 0)
+@variable(cglp, alphaplus[1:dim] >= 0)       #modeling a free variable using two positive variables 
 @variable(cglp, alphaminus[1:dim] >= 0)
 @variable(cglp, beta)
 @variable(cglp, u[1:3] >= 0)
@@ -28,7 +28,10 @@ optimize!(cglp)
 println("The objective value is = ", objective_value(cglp))
 
 for j in 1:dim
-println("alpha$j = ", value(alphaplus[j] - alphaminus[j]))
+println("alpha$j = ", value(alphaplus[j] - alphaminus[j])) 
 end
 
 println("beta = ", value(beta))
+
+#If objective value > 0: Final split cut to be added to the original LP relaxation is (alpha1*x + alpha2*y <= beta)
+#If objective value <= 0: xstar or (x,y) cannot be seperated from conv(p1 U p2).
